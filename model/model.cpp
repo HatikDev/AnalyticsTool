119252d5fbf8718af47355652e84b9f357a3ccb1
Sergey Doroshin <doroshin@rutoken.ru>
2024-06-23 14:39:31 +0300

    Add icons

--- model/model.cpp
+++ model/model.cpp
@@ -1,92 +1,105 @@
 #include "constants.h"
 #include "model.h"
+#include "utils.h"
+
+#include <QIcon>
 
 RectModel::RectModel(QObject* parent)
 {}
 
 int RectModel::rowCount(const QModelIndex& parent) const
 {
-    return m_rects.size();
+	return m_rects.size();
 }
 
 int RectModel::columnCount(const QModelIndex& parent) const
 {
-    return 1;
+	return 1;
 }
 
 QVariant RectModel::data(const QModelIndex& index, int role) const
 {
-    int row = index.row();
-    int column = index.column();
+	int row = index.row();
+	int column = index.column();
 
-    if (row < 0 || row >= m_rects.size() || column != 0 || (role != Qt::DisplayRole && role != Qt::UserRole && role != Qt::CheckStateRole))
-        return QVariant();
+	if (row < 0 || row >= m_rects.size() || column != 0 || (role != Qt::DisplayRole && role != Qt::UserRole
+															&& role != Qt::CheckStateRole && role != Qt::DecorationRole))
+		return QVariant();
 
-    auto it = m_rects.begin();
-    std::advance(it, row);
+	auto it = m_rects.begin();
+	std::advance(it, row);
 
-    if (role == Qt::UserRole)
-        return (*it).second->number();
+	if (role == Qt::UserRole)
+		return (*it).second->number();
 
-    if (role == Qt::CheckStateRole)
-        return (*it).second->isVisible() ? Qt::Checked : Qt::Unchecked;
+	if (role == Qt::CheckStateRole)
+		return (*it).second->isVisible() ? Qt::Checked : Qt::Unchecked;
 
-    return (*it).second->name().c_str();
+	if (role == Qt::DecorationRole)
+	{
+		auto cellType = (*it).second->cellType();
+
+		QPixmap pixmap(15, 15); // TODO: replace by constant
+		pixmap.fill(utils::colorByClass(cellType));
+		return QIcon(pixmap);
+	}
+
+	return (*it).second->name().c_str();
 }
 
 bool RectModel::setData(const QModelIndex& index, const QVariant& value, int role)
 {
-    if (role != Qt::CheckStateRole || index.column() != 0)
-        return false;
+	if (role != Qt::CheckStateRole || index.column() != 0)
+		return false;
 
-    auto rectId = data(index, Qt::UserRole).toInt();
-    if (value == Qt::Checked)
-        m_rects[rectId]->setVisible(true);
-    else if (value == Qt::Unchecked)
-        m_rects[rectId]->setVisible(false);
+	auto rectId = data(index, Qt::UserRole).toInt();
+	if (value == Qt::Checked)
+		m_rects[rectId]->setVisible(true);
+	else if (value == Qt::Unchecked)
+		m_rects[rectId]->setVisible(false);
 
-    emit dataChanged(index, index);
+	emit dataChanged(index, index);
 
-    return true;
+	return true;
 }
 
 void RectModel::addRect(Rectangle* rect)
 {
-    beginInsertRows(QModelIndex(), m_rects.size(), m_rects.size() + 1);
-    m_rects[rect->number()] = rect;
-    endInsertRows();
+	beginInsertRows(QModelIndex(), m_rects.size(), m_rects.size() + 1);
+	m_rects[rect->number()] = rect;
+	endInsertRows();
 }
 
 void RectModel::deleteRect(Rectangle* rect)
 {
-    auto it = m_rects.find(rect->number());
-    if (it == m_rects.end())
-        return;
+	auto it = m_rects.find(rect->number());
+	if (it == m_rects.end())
+		return;
 
-    beginRemoveRows(QModelIndex(), 0, m_rects.size() - 1);
-    m_rects.erase(rect->number());
-    endRemoveRows();
+	beginRemoveRows(QModelIndex(), 0, m_rects.size() - 1);
+	m_rects.erase(rect->number());
+	endRemoveRows();
 }
 
 int RectModel::rowByRect(Rectangle* rect) const
 {
-    auto it = m_rects.find(rect->number());
-    if (it == m_rects.end())
-        return -1;
+	auto it = m_rects.find(rect->number());
+	if (it == m_rects.end())
+		return -1;
 
-    return std::distance(m_rects.begin(), it);
+	return std::distance(m_rects.begin(), it);
 }
 
 Qt::ItemFlags RectModel::flags(const QModelIndex& index) const
 {
-    if (index.column() == 0)
-    {
-        Qt::ItemFlags flags;
-        flags |= Qt::ItemIsSelectable;
-        flags |= Qt::ItemIsEnabled;
-        flags |= Qt::ItemIsUserCheckable;
-        return flags;
-    }
+	if (index.column() == 0)
+	{
+		Qt::ItemFlags flags;
+		flags |= Qt::ItemIsSelectable;
+		flags |= Qt::ItemIsEnabled;
+		flags |= Qt::ItemIsUserCheckable;
+		return flags;
+	}
 
-    return QAbstractItemModel::flags(index);
+	return QAbstractItemModel::flags(index);
 }
--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,37 +1,45 @@
+#include "analyticsexception.h"
 #include "utils.h"
+
+namespace
+{
+    static std::unordered_map<uint8_t, QColor> colors;
+
+    void initColors() {
+        colors.emplace(0, "#00ffff");
+        colors.emplace(1, "#ff0000");
+        colors.emplace(2, "#00ff00");
+        colors.emplace(3, "#ffa500");
+        colors.emplace(4, "#9370db");
+        colors.emplace(5, "#96D996");
+        colors.emplace(6, "#D39B85");
+        colors.emplace(7, "#E13AFF");
+        colors.emplace(8, "#008000");
+        colors.emplace(9, "#FFFF00");
+        colors.emplace(10, "#006464");
+        colors.emplace(11, "#323232");
+        colors.emplace(12, "#B1FF4C");
+        colors.emplace(13, "#6C3B2A");
+        colors.emplace(14, "#B0C4DE");
+        colors.emplace(15, "#BEBD7F");
+        colors.emplace(16, "#B85D43");
+        colors.emplace(17, "#122FAA");
+        colors.emplace(18, "#FCD975");
+        colors.emplace(19, "#8B8940");
+        colors.emplace(20, "#78DBE2");
+        colors.emplace(21, "#A0522D");
+        colors.emplace(22, "#122FAA");
+    }
+}
 
 QColor utils::colorByClass(uint8_t cellClass)
 {
-    switch (cellClass) {
-    case 0:
-        return Qt::red;
-    case 1:
-        return Qt::green;
-    case 2:
-        return Qt::blue;
-    case 3:
-        return QColor(139, 0, 0);
-    case 4:
-        return QColor(173, 255, 47);
-    case 5:
-        return QColor(255, 255, 0);
-    case 6:
-        return QColor(0, 255, 255);
-    case 7:
-        return QColor(70, 130, 180);
-    case 8:
-        return QColor(165, 42, 42);
-    case 9:
-        return QColor(222, 184, 135);
-    case 10:
-        return QColor(25, 25, 112);
-    case 11:
-        return QColor(173, 255, 47);
-    case 12:
-        return QColor(0, 100, 0);
-    case 13:
-        return QColor(128, 128, 0);
-    default:
-        return Qt::black;
-    }
+    if (colors.empty())
+        initColors();
+
+    auto it = colors.find(cellClass);
+    if (it == colors.end())
+        throw AnalyticsException("Can't find color for class " + std::to_string(cellClass));
+
+    return it->second;
 }
